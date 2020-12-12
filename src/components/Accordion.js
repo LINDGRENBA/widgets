8183b294ad8d4b657a341c7022aeb90bfd21efc9
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-12-12 10:47:52 -0800

    Add onClick helper function and initial state.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
-    // return React.Fragment instead of div to avoid double border on accordion which is a side effect of Semantic UI
+
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onAccordionTitleClick = (index) => {
+    setActiveIndex(index);
+  }
+
+  // return React.Fragment instead of div to avoid double border on accordion which is a side effect of Semantic UI
+  const renderedItems = items.map((item, index) => {
     return <React.Fragment key={item.title}>
-      <div className="title active">
+      <div 
+        className="title active"
+        onClick={() => onAccordionTitleClick(index)}
+      >
         <i className="dropdown icon"></i>
         {item.title}
       </div>
@@ -13,7 +23,10 @@ const Accordion = ({ items }) => {
       </div>
     </React.Fragment>
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>;
 };
 
 export default Accordion;
