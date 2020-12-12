5ea8c6a8ec7a231158a7830c56eba5075ff4510c
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-12-12 10:57:06 -0800

    Update Accordion to assign active class dynamically on click.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -10,22 +10,22 @@ const Accordion = ({ items }) => {
 
   // return React.Fragment instead of div to avoid double border on accordion which is a side effect of Semantic UI
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
     return <React.Fragment key={item.title}>
       <div 
-        className="title active"
+        className={`title ${active}`}
         onClick={() => onAccordionTitleClick(index)}
       >
         <i className="dropdown icon"></i>
         {item.title}
       </div>
-      <div className="content active">
+      <div className={`content ${active}`}>
         <p>{item.content}</p>
       </div>
     </React.Fragment>
   });
   return <div className="ui styled accordion">
       {renderedItems}
-      <h1>{activeIndex}</h1>
     </div>;
 };
 
